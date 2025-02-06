cece3a8c57ae6807cad7f26f5d6af9ca9cf575d9
John-D-McGurk <john.d.mcgurk@hotmail.com>
2025-02-06 10:19:51 +0100

    started ass-4

---
+++ assignment-4/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function addContact(form) {
+    console.log(form.get('name'))
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contactForm = document.querySelector("#contact-form");
+
+  console.log(contactForm);
+
+  contactForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addContact(new FormData(contactForm));
+  });
+});
